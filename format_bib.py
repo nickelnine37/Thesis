cab05b7e4d798b49fb1475ff29b0b8455f6f04c8
nickelnine37 <edward.antonian1@gmail.com>
2023-07-11 11:17:36 +0100

    introduction first pass]

--- format_bib.py
+++ format_bib.py
@@ -11,7 +11,7 @@ with open('Bibliography.bib', 'r+') as f:
     bib_database = bibtexparser.load(f)
 
 
-skipitems = ['EPA2023', 'FRED2023', 'ONS2019']
+skipitems = ['EPA2023', 'FRED2023', 'ONS2019', 'Bradbury2018', 'Wang2022b', 'Wang2022c']
 
 for item in bib_database.entries:
 
