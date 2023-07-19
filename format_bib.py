9137d9f69bfa8d1b44319b5b6717f8d793898d53
nickelnine37 <edward.antonian1@gmail.com>
2023-07-19 12:01:00 +0100

    updated lit review

--- format_bib.py
+++ format_bib.py
@@ -11,7 +11,9 @@ with open('Bibliography.bib', 'r+') as f:
     bib_database = bibtexparser.load(f)
 
 
-skipitems = ['EPA2023', 'FRED2023', 'ONS2019', 'Bradbury2018', 'Wang2022b', 'Wang2022c', 'Sandryhaila2013a', 'Sandryhaila2013b']
+skipitems = ['EPA2023', 'FRED2023', 'ONS2019', 'Bradbury2018', 'Wang2022b',         
+             'Wang2022c', 'Sandryhaila2013a', 'Sandryhaila2013b', 'Romero2017b', 
+             'Zhou2022b', 'Wang2015b', 'Narang2013b']
 
 for item in bib_database.entries:
 
